8bda8dd8d600dfb999569ce8ef3991891d0c0a44
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-26 17:38:53 +0900

    1026

--- HELLO_PYTHON/day01/myarr.py
+++ HELLO_PYTHON/day01/myarr.py
@@ -9,6 +9,9 @@ arr = ["홍길동", "전우치", "이순신"]
 print(arr)
 arr.append("홍범도")
 print(arr)
-arr.insert(1, "도요토미")
+arr.insert(len(arr), "도요토미") #insert 사용하여 append 기능까지 실행 가능
 print(arr)
+
+print(arr[0])
+print(arr[-1]) #자바에서는 음수를 넣으면 오류 발생
 
