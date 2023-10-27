7d93f9a737f5aebde79591422aa2e01d09f72890
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-27 14:22:01 +0900

    1027

---
+++ HELLO_PYTHON/day02/myrange.py
@@ -0,0 +1,23 @@
+arr = range(5)
+
+for i in arr:
+    print(i)
+    
+print()
+    
+arr1 = range(0,5)
+
+for i in arr1:
+    print(i)
+    
+print()
+    
+arr2 = range(0,4+1) #가독성을 위한 표현
+
+for i in arr2:
+    print(i)
+
+print()    
+    
+print(arr[0])
+print(arr[4])             
---
+++ HELLO_PYTHON/day02/mytest01.py
@@ -0,0 +1,13 @@
+# 1~10까지의 합을 구하시오.
+# for, range 이용
+
+arr = range(0,11)
+mysum = 0
+
+for i in arr:
+    mysum += i
+print(mysum)
+
+
+
+
