b88b9d2673aa2335b7cbed0f101b14f774ce350d
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-30 14:25:16 +0900

    1030

---
+++ HELLO_PYTHON/day03/mydef01.py
@@ -0,0 +1,25 @@
+def add(a,b):
+    return a+b
+
+def minus(a,b):
+    return a-b
+
+def mutiply(a,b):
+    return a*b
+
+def divide(a,b):
+    return a/b
+
+sum = add(4,2)
+min = minus(4,2)
+mul = mutiply(4,2)
+div = divide(4,2)
+
+print("sum :",sum)
+print("min :",min)
+print("mul :",mul)
+print("div :",div)
+
+
+
+
