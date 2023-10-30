751a7c2a5274cf0743e84ca51f4b8f38d4af19a7
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-30 14:25:54 +0900

    1030

---
+++ HELLO_JAVA/src/day03/MyMethod01.java
@@ -0,0 +1,31 @@
+package day03;
+
+public class MyMethod01 {
+
+	static int add(int a, int b) {
+		return a+b;
+	}
+	static int minus(int a, int b) {
+		return a-b;
+	}
+	static int multply(int a, int b) {
+		return a*b;
+	}
+	static int divice(int a, int b) {
+		return a/b;
+	}
+	
+	
+	public static void main(String[] args) {
+		int sum = add(4,2);
+		System.out.println("sum : "+sum);
+		int min = minus(4,2);
+		System.out.println("min : "+min);
+		int mul = multply(4,2);
+		System.out.println("mul : "+mul);
+		int div = divice(4,2);
+		System.out.println("div : "+div);		
+
+	}
+
+}
