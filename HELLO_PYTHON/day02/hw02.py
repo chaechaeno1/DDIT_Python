3d18cf9a41bcdfc482d39275c32cc6c0d3abaa9f
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-30 14:09:51 +0900

    1030

--- HELLO_PYTHON/day02/hw02.py
+++ HELLO_PYTHON/day02/hw02.py
@@ -1,7 +1,7 @@
 #업앤다운
 #com = 1~99사이의 랜덤수 (화면X) - 50 이라 가정
 #입력할 수를 넣으세요. 40
-# 40 UP입니다.
+# 40 UP 입니다.
 # 입력할 수를 넣으세요.60
 # 60 DOWN 입니다.
 # 입력할 수를 넣으세요.50
@@ -9,15 +9,17 @@
 # 물어보는 횟수는 10번으로 제한
 from random import random
 
-com = int(random() * 100)
+com = int(random() * 99)+1
 
-for i in range(1,10+1) :
-    mine = input("입력할 수를 넣으세요.")
-    intmine = int(mine)
-    if intmine > com : print(mine + " DOWN 입니다.")
-    elif intmine < com : print(mine + " UP 입니다.")
+for i in range(10):
+    mine = input("입력할 수를 넣으세요.") #문자열
+    intmine = int(mine) #정수형
+    if intmine > com : 
+        print(mine + " DOWN 입니다.")
+    elif intmine < com : 
+        print(mine + " UP 입니다.")
     else : 
-        print(mine + "정답 입니다.")
+        print(mine + " 정답 입니다.")
         break 
              
 
