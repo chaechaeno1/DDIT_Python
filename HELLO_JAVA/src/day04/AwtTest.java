9bc6215a85ff51c6f9ed66a8fad5518698d6bda1
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-10-31 15:05:40 +0900

    1031

---
+++ HELLO_JAVA/src/day04/AwtTest.java
@@ -0,0 +1,12 @@
+package day04;
+
+import java.awt.Frame;
+
+public class AwtTest {
+	public static void main(String[] args) {
+		Frame f = new Frame(); // 보통 Frame을 window로 생각하면 됨
+		f.setVisible(true);
+		f.setSize(300, 400);
+	}
+
+}
