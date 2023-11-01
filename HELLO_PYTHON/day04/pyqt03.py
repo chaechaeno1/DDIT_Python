14b29eb9527fe8f2cb02a1a7481e39cf3d219577
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-11-01 17:32:18 +0900

    1101

--- HELLO_PYTHON/day04/pyqt03.py
+++ HELLO_PYTHON/day04/pyqt03.py
@@ -2,7 +2,7 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-form_class = uic.loadUiType("pyqt02.ui")[0]
+form_class = uic.loadUiType("pyqt03.ui")[0]
 
 class MainClass(QMainWindow, form_class):
     def __init__(self) :
@@ -12,10 +12,14 @@ class MainClass(QMainWindow, form_class):
         self.show()
         
     def myclick(self) :
-        txt = self.le.text()
-        num = float(txt)
-        txt2 = str(num*2)
-        self.le.setText(txt2)
+        num1 = self.te1.toPlainText()
+        num2 = self.te2.toPlainText()
+        
+        inum1 = int(num1)
+        inum2 = int(num2)
+                
+        res = str(inum1-inum2)
+        self.te3.setText(res)
         
 if __name__ == "__main__" :
     app = QApplication(sys.argv) 
