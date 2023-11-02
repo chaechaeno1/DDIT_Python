b4f0200113bee083ae5c32627ad863cdb145826a
CHAE <110103267+chaechaeno1@users.noreply.github.com>
2023-11-02 14:13:06 +0900

    1102

--- HELLO_JAVA/src/day04/MySwing9_class.java
+++ HELLO_JAVA/src/day04/MySwing9_class.java
@@ -15,7 +15,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
-public class MySwing09 extends JFrame {
+public class MySwing9_class extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField tf;
@@ -27,7 +27,7 @@ public class MySwing09 extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MySwing09 frame = new MySwing09();
+					MySwing9_class frame = new MySwing9_class();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -39,7 +39,7 @@ public class MySwing09 extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public MySwing09() {
+	public MySwing9_class() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 313, 316);
 		contentPane = new JPanel();
@@ -56,10 +56,10 @@ public class MySwing09 extends JFrame {
 		
 		JButton btn1 = new JButton("1");
 		btn1.addMouseListener(new MouseAdapter() {
+			
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "1");
-				
+				myclick(e);
 			}
 		});
 		btn1.setBounds(35, 60, 70, 23);
@@ -67,10 +67,12 @@ public class MySwing09 extends JFrame {
 		
 		JButton btn2 = new JButton("2");
 		btn2.addMouseListener(new MouseAdapter() {
+			
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "2");
+				myclick(e);
 			}
+			
 		});
 		btn2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -83,8 +85,9 @@ public class MySwing09 extends JFrame {
 		btn3.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "3");
+				myclick(e);
 			}
+			
 		});
 		btn3.setBounds(184, 60, 70, 23);
 		contentPane.add(btn3);
@@ -93,8 +96,9 @@ public class MySwing09 extends JFrame {
 		btn4.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "4");
+				myclick(e);
 			}
+			
 		});
 		btn4.setBounds(35, 93, 70, 23);
 		contentPane.add(btn4);
@@ -103,8 +107,9 @@ public class MySwing09 extends JFrame {
 		btn5.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "5");
+				myclick(e);
 			}
+			
 		});
 		btn5.setBounds(110, 93, 70, 23);
 		contentPane.add(btn5);
@@ -113,8 +118,9 @@ public class MySwing09 extends JFrame {
 		btn6.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "6");
+				myclick(e);
 			}
+			
 		});
 		btn6.setBounds(184, 93, 70, 23);
 		contentPane.add(btn6);
@@ -123,8 +129,9 @@ public class MySwing09 extends JFrame {
 		btn7.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "7");
+				myclick(e);
 			}
+			
 		});
 		btn7.setBounds(35, 126, 70, 23);
 		contentPane.add(btn7);
@@ -133,8 +140,9 @@ public class MySwing09 extends JFrame {
 		btn8.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "8");
+				myclick(e);
 			}
+			
 		});
 		btn8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -147,8 +155,9 @@ public class MySwing09 extends JFrame {
 		btn9.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "9");
+				myclick(e);
 			}
+			
 		});
 		btn9.setBounds(184, 126, 70, 23);
 		contentPane.add(btn9);
@@ -157,8 +166,9 @@ public class MySwing09 extends JFrame {
 		btn0.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				tf.setText(tf.getText() + "0");
+				myclick(e);
 			}
+			
 		});
 		btn0.setBounds(35, 159, 70, 23);
 		contentPane.add(btn0);
@@ -167,11 +177,13 @@ public class MySwing09 extends JFrame {
 		
 		JButton btn_call = new JButton("☎");
 		btn_call.addMouseListener(new MouseAdapter() {
+
+			
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				JOptionPane aa = new JOptionPane();
-				aa.showMessageDialog(null, "Calling to "+tf.getText());
 				
+				mycall();
+
 			}
 		});
 		btn_call.addActionListener(new ActionListener() {
@@ -181,5 +193,21 @@ public class MySwing09 extends JFrame {
 		btn_call.setBounds(110, 159, 144, 23);
 		contentPane.add(btn_call);
 	}
+	
+	
+	void myclick(MouseEvent e) {
+		JButton temp =  (JButton) e.getComponent();
+		String str_new = temp.getText();
+		
+		String str_old = tf.getText();
+		tf.setText(str_old+str_new);
+		
+	}
+	void mycall() {
+		String str_tel = tf.getText();
+		JOptionPane.showMessageDialog(null, "Calling to\n"+str_tel);
+		
+	}
+	
 
 }
--- HELLO_PYTHON/day04/pyqt04.py
+++ HELLO_PYTHON/day04/pyqt04.py
@@ -1,7 +1,9 @@
+from random import random
 import sys
+
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from random import random
+
 
 form_class = uic.loadUiType("pyqt04.ui")[0]
 
@@ -14,23 +16,22 @@ class MainClass(QMainWindow, form_class):
         
     def myclick(self) :
         arr = list(range(1,45+1))
+        
         for i in range(1000):
-            rnd = int(random()*45)
+            rnd = int(random() * 45)
             
             temp = arr[0]
             arr[0] = arr[rnd]
             arr[rnd] = temp
                         
-        self.pte1.setText(arr[0]);
-        self.pte2.setText(arr[1]); 
-        self.pte3.setText(arr[2]); 
-        self.pte4.setText(arr[3]); 
-        self.pte5.setText(arr[4]); 
-        self.pte6.setText(arr[5]);                 
+        self.pte1.setPlainText(str(arr[0]));
+        self.pte2.setPlainText(str(arr[1])); 
+        self.pte3.setPlainText(str(arr[2])); 
+        self.pte4.setPlainText(str(arr[3])); 
+        self.pte5.setPlainText(str(arr[4])); 
+        self.pte6.setPlainText(str(arr[5]));                 
     
-        
-        
-        
+
 if __name__ == "__main__" :
     app = QApplication(sys.argv) 
     window = MainClass() 
